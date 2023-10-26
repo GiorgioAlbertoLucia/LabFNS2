c598119a2b0c785aea73d74ab0e42785f2093be4
Roberta-Ferioli <roberta.ferioli@edu.unito.it>
2023-10-26 16:20:46 +0200

    add .h

---
+++ Beta/src/analysis.h
@@ -0,0 +1,147 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Mon Jun  8 16:26:28 2020 by ROOT version 6.14/06
+// from TTree wfm/wfm
+// found on file: BetaMeas_Lab2.root
+//////////////////////////////////////////////////////////
+
+#ifndef analysis_h
+#define analysis_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+#include "vector"
+
+class analysis {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+   // Declaration of leaf types
+   Int_t           event;
+   vector<double>  *w2;
+   vector<double>  *t2;
+   vector<double>  *w3;
+   vector<double>  *t3;
+
+   // List of branches
+   TBranch        *b_event;   //!
+   TBranch        *b_w2;   //!
+   TBranch        *b_t2;   //!
+   TBranch        *b_w3;   //!
+   TBranch        *b_t3;   //!
+
+   analysis(TTree *tree=0);
+   virtual ~analysis();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef analysis_cxx
+analysis::analysis(TTree *tree) : fChain(0) 
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("BetaMeas_Lab2.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("BetaMeas_Lab2.root");
+      }
+      f->GetObject("wfm",tree);
+
+   }
+   Init(tree);
+}
+
+analysis::~analysis()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t analysis::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t analysis::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void analysis::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set object pointer
+   w2 = 0;
+   t2 = 0;
+   w3 = 0;
+   t3 = 0;
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("event", &event, &b_event);
+   fChain->SetBranchAddress("w2", &w2, &b_w2);
+   fChain->SetBranchAddress("t2", &t2, &b_t2);
+   fChain->SetBranchAddress("w3", &w3, &b_w3);
+   fChain->SetBranchAddress("t3", &t3, &b_t3);
+   Notify();
+}
+
+Bool_t analysis::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void analysis::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t analysis::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef analysis_cxx
