9348b8a1aa6e8a3c89e9100306c0fef31dde02b7
Roberta-Ferioli <roberta.ferioli@edu.unito.it>
2023-10-31 15:12:12 +0100

    Fix errors on Gain vs Bias

--- TCT/src/Create_Gain_vs_Bias.py
+++ TCT/src/Create_Gain_vs_Bias.py
@@ -26,18 +26,18 @@ if __name__=='__main__':
     dfLGAD5 = pd.read_csv(infileLGAD5,comment='#')
 
     dfPiN['QPin']=(dfPiN['AtotPin']-dfPiN['AbasPin'])
-    dfPiN['QPin_err']=(dfPiN['Atot_errPin']+dfPiN['Abas_errPin'])
+    dfPiN['QPin_err']=(np.sqrt((dfPiN['Atot_errPin']/np.sqrt(1000))**2+(dfPiN['Abas_errPin']/np.sqrt(1000))**2))
     dfLGAD['QLGAD']=(dfLGAD['AtotLGAD']-dfLGAD['AbasLGAD'])
-    dfLGAD['QLGAD_err']=(dfLGAD['Atot_errLGAD']+dfLGAD['Abas_errLGAD'])
+    dfLGAD['QLGAD_err']=(np.sqrt((dfLGAD['Atot_errLGAD']/np.sqrt(1000))**2+(dfLGAD['Abas_errLGAD']/np.sqrt(1000))**2))
 
     dfPiN5['QPin']=(dfPiN5['AtotPin']-dfPiN5['AbasPin'])
-    dfPiN5['QPin_err']=(dfPiN5['Atot_errPin']+dfPiN5['Abas_errPin'])
+    dfPiN5['QPin_err']=(np.sqrt((dfPiN5['Atot_errPin']/np.sqrt(1000))**2+(dfPiN5['Abas_errPin']/np.sqrt(1000))**2))
     dfLGAD5['QLGAD']=(dfLGAD5['AtotLGAD']-dfLGAD5['AbasLGAD'])
-    dfLGAD5['QLGAD_err']=(dfLGAD5['Atot_errLGAD']+dfLGAD5['Abas_errLGAD'])
+    dfLGAD5['QLGAD_err']=(np.sqrt((dfLGAD5['Atot_errLGAD']/np.sqrt(1000))**2+(dfLGAD5['Abas_errLGAD']/np.sqrt(1000))**2))
 
     df = pd.DataFrame()
     df['Gain']=dfLGAD['QLGAD']/dfPiN['QPin']
-    df['Gain_err']=np.sqrt((dfLGAD['QLGAD']/(dfPiN['QPin']*dfPiN['QPin'])*dfPiN['QPin_err'])**2+ (dfLGAD['QLGAD_err']/dfPiN['QPin'])**2)
+    df['Gain_err']=np.sqrt(((dfLGAD['QLGAD']/(dfPiN['QPin']*dfPiN['QPin']))*dfPiN['QPin_err'])**2+ (dfLGAD['QLGAD_err']/dfPiN['QPin'])**2)
     df['Bias'] = dfPiN['Bias']
     df['Bias_err'] = dfPiN['Bias_err']
     #commento a caso p
@@ -45,7 +45,7 @@ if __name__=='__main__':
 
     df5 = pd.DataFrame()
     df5['Gain']=dfLGAD5['QLGAD']/dfPiN5['QPin']
-    df5['Gain_err']=np.sqrt((dfLGAD5['QLGAD']/(dfPiN5['QPin']*dfPiN5['QPin'])*dfPiN5['QPin_err'])**2+ (dfLGAD5['QLGAD_err']/dfPiN5['QPin'])**2)
+    df5['Gain_err']=np.sqrt(((dfLGAD5['QLGAD']/(dfPiN5['QPin']*dfPiN5['QPin']))*dfPiN5['QPin_err'])**2+ (dfLGAD5['QLGAD_err']/dfPiN5['QPin'])**2)
     df5['Bias'] = dfPiN5['Bias']
     df5['Bias_err'] = dfPiN5['Bias_err']
 
@@ -68,7 +68,7 @@ if __name__=='__main__':
     gGain5.SetLineColor(kAzure)
     canvas = TCanvas("canvas","canvas",2100,1000)
     canvas.cd()
-    hFrame = canvas.cd().DrawFrame(140,0,280,500,"Gain vs Bias")
+    hFrame = canvas.cd().DrawFrame(140,0,280,500,"Gain vs Bias; Reverse Bias (V); Gain")
     gGain.Draw("same,p")
     gGain5.Draw("same,p")
     legend1 = TLegend(0.25, 0.5, 0.55, 0.83)
