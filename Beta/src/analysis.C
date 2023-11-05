7f27f1a5435a057451586c1912f07e4f8f45642c
Roberta-Ferioli <roberta.ferioli@edu.unito.it>
2023-11-05 13:37:06 +0100

    create TTree output

--- Beta/src/analysis.C
+++ Beta/src/analysis.C
@@ -11,12 +11,28 @@ void analysis::Loop()
 {
 
    if (fChain == 0) return;
-   TH1D * histoAmpli2 = new TH1D("histoAmpli2","histoAmpli2",200,0.,400); //mV
-   TH1D * histoAmpli3 = new TH1D("histoAmpli3","histoAmpli3",200,0.,400);
-   TH1D * histoToA2 = new TH1D("histoToA2","histoToA2",200,0.,2.5); //ns
-   TH1D * histoToA3 = new TH1D("histoToA3","histoToA3",200,0.,2.5);
-   TH1D * histoRMS2 = new TH1D("histoRMS2","histoRMS2",200,0.,1); //mV
-   TH1D * histoRMS3 = new TH1D("histoRMS3","histoRMS3",200,0.,1);
+   double Amp2{0.}, Amp3{0.};
+   double ToA2{0.}, ToA3{0.};
+   double RMS2{0.}, RMS3{0.};
+   double baseline2{0.}, baseline3{0.};
+    TFile outFile("BetaOutput.root", "recreate");
+    cout<<"cout1"<<endl;
+   TTree * tree = new TTree("BetaTree","BetaTree");
+   cout<<"cout1"<<endl;
+   tree->Branch("Amp2", &Amp2);   
+   tree->Branch("Amp3", &Amp3); 
+   tree->Branch("RMS2", &RMS2); 
+   tree->Branch("RMS3", &RMS3); 
+   tree->Branch("ToA2", &ToA2); 
+   tree->Branch("ToA3", &ToA3);       
+   tree->Branch("baseline2", &baseline2); 
+   tree->Branch("baseline3", &baseline3);                     
+   //TH1D * histoAmpli2 = new TH1D("histoAmpli2","histoAmpli2",200,0.,400); //mV
+   //TH1D * histoAmpli3 = new TH1D("histoAmpli3","histoAmpli3",200,0.,400);
+   //TH1D * histoToA2 = new TH1D("histoToA2","histoToA2",200,0.,2.5); //ns
+   //TH1D * histoToA3 = new TH1D("histoToA3","histoToA3",200,0.,2.5);
+   //TH1D * histoRMS2 = new TH1D("histoRMS2","histoRMS2",200,0.,1); //mV
+   //TH1D * histoRMS3 = new TH1D("histoRMS3","histoRMS3",200,0.,1);
    Long64_t nentries = fChain->GetEntriesFast();
 
    Long64_t nbytes = 0, nb = 0;
@@ -27,12 +43,8 @@ void analysis::Loop()
 
       // Il vostro codice va qui
       const int numPoints=w2->size();
-      double baseLine2{0.}, baseLine3{0.};
-      double RMS2{0.}, RMS3{0.};
       TH1D* histoNoise2 = new TH1D("histoNoise2","histoNoise2",200,-1.,1.);
       TH1D* histoNoise3 = new TH1D("histoNoise3","histoNoise3",200,-1.,1.);
-      double amplitude2{0.}, amplitude3{0.};
-      double toA2{0.}, toA3{0.};
       for(int ii=0;ii<numPoints;ii++)
       {
          if(ii>20 && ii<220)
@@ -41,36 +53,41 @@ void analysis::Loop()
             //cout<<"cout3"<<endl;
             histoNoise3->Fill(w3->at(ii)*1000);
          }
-         if(w2->at(ii)*1000>amplitude2)
+         if(w2->at(ii)*1000>Amp2)
          {
-            amplitude2=w2->at(ii)*1000;
-            toA2=t2->at(ii)*1000000000;
+            Amp2=w2->at(ii)*1000;
+            ToA2=t2->at(ii)*1000000000;
          }
-             if(w3->at(ii)*1000>amplitude3)
+             if(w3->at(ii)*1000>Amp3)
          {
-            amplitude3=w3->at(ii)*1000;
-            toA3=t3->at(ii)*1000000000;
+            Amp3=w3->at(ii)*1000;
+            ToA3=t3->at(ii)*1000000000;
          }
       }
       
-      baseLine2=histoNoise2->GetMean();
-      baseLine3=histoNoise3->GetMean();
+      baseline2=histoNoise2->GetMean();
+      baseline3=histoNoise3->GetMean();
       RMS2=histoNoise2->GetRMS();
       RMS3=histoNoise3->GetRMS();
-      histoAmpli2->Fill(amplitude2);
-      histoAmpli3->Fill(amplitude3);
-      histoToA2->Fill(toA2);
-      histoToA3->Fill(toA3);
-      histoRMS2->Fill(RMS2);
-      histoRMS3->Fill(RMS3);
+      //histoAmpli2->Fill(amplitude2);
+      //histoAmpli3->Fill(amplitude3);
+      //histoToA2->Fill(toA2);
+      //histoToA3->Fill(toA3);
+      //histoRMS2->Fill(RMS2);
+      //histoRMS3->Fill(RMS3);
       delete histoNoise2;
       delete histoNoise3;
-
+      tree->Fill();
       nbytes += nb;
    }
+    
+    tree->Write();
+    //outFile.Close();
 
+    //delete outptfile;
+    delete tree;
 // Gli istogrammi vanno fittati e plottati qui
-TCanvas * canvasA = new TCanvas("canvasA","canvasA",1000,1000);
+/*TCanvas * canvasA = new TCanvas("canvasA","canvasA",1000,1000);
 canvasA->Divide(2,1);
 canvasA->cd(1);
 histoAmpli2->Draw("hist");
@@ -110,7 +127,7 @@ histoRMS3->Draw("hist");
 histoRMS3->SetLineColor(kRed);
 histoRMS3->GetXaxis()->SetTitle("Baseline RMS (mV)"); 
 histoRMS3->GetYaxis()->SetTitle("Entries (a.u.)");
-histoRMS3->SetTitle("Sensor B - Channel 3");
+histoRMS3->SetTitle("Sensor B - Channel 3");*/
 
 
 }
---
+++ Beta/src/analysis_C.d
@@ -0,0 +1,73 @@
+
+# DO NOT DELETE
+
+./analysis_C.so: analysis.h /mnt/c/root/include/TROOT.h
+./analysis_C.so: /mnt/c/root/include/TDirectory.h
+./analysis_C.so: /mnt/c/root/include/TNamed.h /mnt/c/root/include/TObject.h
+./analysis_C.so: /mnt/c/root/include/Rtypes.h
+./analysis_C.so: /mnt/c/root/include/RtypesCore.h
+./analysis_C.so: /mnt/c/root/include/ROOT/RConfig.hxx
+./analysis_C.so: /mnt/c/root/include/RVersion.h
+./analysis_C.so: /mnt/c/root/include/RConfigure.h
+./analysis_C.so: /mnt/c/root/include/DllImport.h /mnt/c/root/include/strtok.h
+./analysis_C.so: /mnt/c/root/include/strlcpy.h /mnt/c/root/include/snprintf.h
+./analysis_C.so: /mnt/c/root/include/TGenericClassInfo.h
+./analysis_C.so: /mnt/c/root/include/TSchemaHelper.h
+./analysis_C.so: /mnt/c/root/include/TIsAProxy.h
+./analysis_C.so: /mnt/c/root/include/TVirtualIsAProxy.h
+./analysis_C.so: /mnt/c/root/include/TStorage.h
+./analysis_C.so: /mnt/c/root/include/TVersionCheck.h
+./analysis_C.so: /mnt/c/root/include/RVersion.h /mnt/c/root/include/TString.h
+./analysis_C.so: /mnt/c/root/include/TMathBase.h
+./analysis_C.so: /mnt/c/root/include/ROOT/RStringView.hxx
+./analysis_C.so: /mnt/c/root/include/ROOT/TypeTraits.hxx
+./analysis_C.so: /mnt/c/root/include/TClass.h
+./analysis_C.so: /mnt/c/root/include/TDictionary.h
+./analysis_C.so: /mnt/c/root/include/ESTLType.h
+./analysis_C.so: /mnt/c/root/include/TObjArray.h
+./analysis_C.so: /mnt/c/root/include/TSeqCollection.h
+./analysis_C.so: /mnt/c/root/include/TCollection.h
+./analysis_C.so: /mnt/c/root/include/TIterator.h
+./analysis_C.so: /mnt/c/root/include/TVirtualRWMutex.h
+./analysis_C.so: /mnt/c/root/include/TVirtualMutex.h
+./analysis_C.so: /mnt/c/root/include/TUUID.h /mnt/c/root/include/TList.h
+./analysis_C.so: /mnt/c/root/include/TBuffer.h
+./analysis_C.so: /mnt/c/root/include/TDataType.h /mnt/c/root/include/Bytes.h
+./analysis_C.so: /mnt/c/root/include/TChain.h /mnt/c/root/include/TTree.h
+./analysis_C.so: /mnt/c/root/include/Compression.h
+./analysis_C.so: /mnt/c/root/include/ROOT/TIOFeatures.hxx
+./analysis_C.so: /mnt/c/root/include/TArrayD.h /mnt/c/root/include/TArray.h
+./analysis_C.so: /mnt/c/root/include/TArrayI.h /mnt/c/root/include/TAttFill.h
+./analysis_C.so: /mnt/c/root/include/TAttLine.h
+./analysis_C.so: /mnt/c/root/include/TAttMarker.h
+./analysis_C.so: /mnt/c/root/include/TVirtualTreePlayer.h
+./analysis_C.so: /mnt/c/root/include/TBranch.h
+./analysis_C.so: /mnt/c/root/include/TBranchCacheInfo.h
+./analysis_C.so: /mnt/c/root/include/TBits.h /mnt/c/root/include/TFile.h
+./analysis_C.so: /mnt/c/root/include/TDirectoryFile.h
+./analysis_C.so: /mnt/c/root/include/TDatime.h /mnt/c/root/include/TUrl.h
+./analysis_C.so: /mnt/c/root/include/ROOT/RConcurrentHashColl.hxx
+./analysis_C.so: /mnt/c/root/include/ROOT/TRWSpinLock.hxx
+./analysis_C.so: /mnt/c/root/include/ROOT/TSpinMutex.hxx
+./analysis_C.so: /mnt/c/root/include/TH2.h /mnt/c/root/include/TH1.h
+./analysis_C.so: /mnt/c/root/include/TAxis.h /mnt/c/root/include/TAttAxis.h
+./analysis_C.so: /mnt/c/root/include/TArrayC.h /mnt/c/root/include/TArrayS.h
+./analysis_C.so: /mnt/c/root/include/TArrayF.h /mnt/c/root/include/Foption.h
+./analysis_C.so: /mnt/c/root/include/ROOT/EExecutionPolicy.hxx
+./analysis_C.so: /mnt/c/root/include/TVectorFfwd.h
+./analysis_C.so: /mnt/c/root/include/TVectorDfwd.h
+./analysis_C.so: /mnt/c/root/include/TFitResultPtr.h
+./analysis_C.so: /mnt/c/root/include/TMatrixFBasefwd.h
+./analysis_C.so: /mnt/c/root/include/TMatrixDBasefwd.h
+./analysis_C.so: /mnt/c/root/include/TStyle.h /mnt/c/root/include/TAttText.h
+./analysis_C.so: /mnt/c/root/include/TColor.h /mnt/c/root/include/TCanvas.h
+./analysis_C.so: /mnt/c/root/include/TPad.h /mnt/c/root/include/TVirtualPad.h
+./analysis_C.so: /mnt/c/root/include/TAttPad.h /mnt/c/root/include/TQObject.h
+./analysis_C.so: /mnt/c/root/include/TVirtualQConnection.h
+./analysis_C.so: /mnt/c/root/include/TInterpreter.h
+./analysis_C.so: /mnt/c/root/include/TInterpreterValue.h
+./analysis_C.so: /mnt/c/root/include/GuiTypes.h /mnt/c/root/include/Buttons.h
+./analysis_C.so: /mnt/c/root/include/TAttBBox2D.h
+./analysis_C.so: /mnt/c/root/include/TAttCanvas.h
+./analysis_C.so: /mnt/c/root/include/RVersion.h /mnt/c/root/include/RConfig.h /mnt/c/root/include/TClass.h /mnt/c/root/include/TDictAttributeMap.h /mnt/c/root/include/TInterpreter.h /mnt/c/root/include/TROOT.h /mnt/c/root/include/TBuffer.h /mnt/c/root/include/TMemberInspector.h /mnt/c/root/include/TError.h /mnt/c/root/include/RtypesImp.h /mnt/c/root/include/TIsAProxy.h /mnt/c/root/include/TFileMergeInfo.h /mnt/c/root/include/TCollectionProxyInfo.h /mnt/c/root/bin/rootcling
+analysis_C__ROOTBUILDVERSION= 6.24/02
