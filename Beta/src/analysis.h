8ce09c713226f5b4e91a7d285b9cc0361da4b6f1
Bizzzio <fachi2001@gmail.com>
2023-11-05 15:59:16 +0100

    Now script can be ran from main directory

--- Beta/src/analysis.h
+++ Beta/src/analysis.h
@@ -55,9 +55,9 @@ analysis::analysis(TTree *tree) : fChain(0)
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
    if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("BetaMeas_Lab2.root");
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("Beta/data/input/BetaMeas_Lab2.root");
       if (!f || !f->IsOpen()) {
-         f = new TFile("BetaMeas_Lab2.root");
+         f = new TFile("Beta/data/input/BetaMeas_Lab2.root");
       }
       f->GetObject("wfm",tree);
 
