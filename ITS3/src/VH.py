ec66287af6235d0524fba7cb5006be7edbb191ed
Roberta-Ferioli <roberta.ferioli@edu.unito.it>
2024-01-03 16:21:28 +0100

    add fit on VH graph

--- ITS3/src/VH.py
+++ ITS3/src/VH.py
@@ -11,7 +11,7 @@ import uproot
 import sys
 sys.path.append('utils')
 
-from ROOT import TGraphErrors, TFile, TCanvas, kRed, kAzure, kOrange, kFullCircle, gROOT, TLatex, gStyle, TLegend, kBlack, kFullSquare,kFullTriangleUp, kFullTriangleDown, TH1D, kGreen, kViolet
+from ROOT import TGraphErrors, TFile, TCanvas, kRed, kAzure, kOrange, kFullCircle, gROOT, TLatex, gStyle, TLegend, kBlack, kFullSquare,kFullTriangleUp, kFullTriangleDown, TH1D, kGreen, kViolet,TF1
 from StyleFormatter import SetObjectStyle, SetGlobalStyle
 
 def SetGraph(graph, name, title, color, marker):
@@ -91,7 +91,7 @@ if __name__=='__main__':
 
     #inner pixel2 if you have txt file with dato of all pixel, one txt file for VH value
     
-    colorArr = [kRed , kAzure, kBlack, kOrange-3]
+    '''colorArr = [kRed , kAzure, kBlack, kOrange-3]
     markerArr = [kFullCircle, kFullSquare, kFullTriangleUp, kFullTriangleDown]
     pixelArr=np.array([])
     VH2=np.array([])
@@ -132,20 +132,20 @@ if __name__=='__main__':
         AmgraphArray=np.append(AmgraphArray,TGraphErrors(len(VH2), np.asarray(VH2, dtype=float), np.asarray(amplitude, dtype=float), np.zeros(len(VH2)), np.asarray(amplitude_err, dtype=float)))
         FallgraphArray=np.append(FallgraphArray,TGraphErrors(len(VH2), np.asarray(VH2, dtype=float), np.asarray(falltime, dtype=float), np.zeros(len(VH2)), np.asarray(falltime_err, dtype=float)))
     
-    canvas2 = TCanvas("canvas2","canvas2",2450,700)
+    canvas2 = TCanvas("canvas2","canvas2",2400,700)
     canvas2.Divide(3,1)
     canvas2.cd(1)
     hFrame = canvas2.cd(1).DrawFrame(0.3,200,1.3,280,"Baseline vs VH; VH (V); Baseline (mV)")
     legend = TLegend(0.15,0.65,0.4,0.9)
-    text =TLatex(0.45, 0.8,"APTS_AO10P_B6")
+    text =TLatex(0.45, 0.8,"APTS_AO10P_B10")
     text2 =TLatex(0.45, 0.73,"Inner Pixels")
     text.SetNDC()
     text2.SetTextSize(gStyle.GetTextSize())
     text2.SetTextFont(42)
     text2.SetNDC()
-    text1 =TLatex(0.45, 0.4,"APTS_AO10P_B6")
+    text1 =TLatex(0.45, 0.4,"APTS_AO10P_B10")
     text3 =TLatex(0.45, 0.33,"Inner Pixels")
-    text4 =TLatex(0.45, 0.85,"APTS_AO10P_B6")
+    text4 =TLatex(0.45, 0.85,"APTS_AO10P_B10")
     text5 =TLatex(0.45, 0.78,"Inner Pixels")
     text1.SetNDC()
     text3.SetTextSize(gStyle.GetTextSize())
@@ -182,11 +182,13 @@ if __name__=='__main__':
     legend3.Draw()
     text4.Draw()
     text5.Draw()
-    canvas2.SaveAs('ITS3/data/output/VH2.pdf')
+    canvas2.SaveAs('ITS3/data/output/VH2.png')'''
     
-    '''#outer pixels
+    #outer pixels
     colorArr =[kGreen+1,kViolet,kViolet+1,kGreen+3,kViolet-1,kAzure,kOrange -3,kViolet -4,kViolet +10,kBlack,kRed,kViolet-9,kGreen -2,kViolet -6,kViolet +7,kGreen -7]
+    colorArr2=[kRed,kAzure,kBlack,kOrange -3]
     markerArr = 4*[kFullCircle, kFullSquare, kFullTriangleUp, kFullTriangleDown]
+    pixelArr2=np.array([])
     VHex=np.array([])
     AmpliPx=[]
     BasePx=[]
@@ -258,9 +260,9 @@ if __name__=='__main__':
                 legendb.AddEntry(BasgraphArray[i], "Pixel "+str(int(i)))
     legend.Draw()
     legendb.Draw()
-    text =TLatex(0.58, 0.8,"APTS_AO10P_B6")
+    text =TLatex(0.58, 0.8,"APTS_AO10P_B10")
     text2 =TLatex(0.58, 0.73,"External Pixels")
-    text1 =TLatex(0.58, 0.4,"APTS_AO10P_B6")
+    text1 =TLatex(0.58, 0.4,"APTS_AO10P_B10")
     text3 =TLatex(0.58, 0.33,"External Pixels")
     text.SetNDC()
     text.SetTextSize(gStyle.GetTextSize())
@@ -289,4 +291,111 @@ if __name__=='__main__':
     text3.SetTextSize(gStyle.GetTextSize())
     text1.Draw()
     text3.Draw()
-    canvas3.SaveAs('ITS3/data/output/VH_external_pixel2.pdf')'''
+    canvas3.SaveAs('ITS3/data/output/VH_external_pixel2.pdf')
+
+    dir_path2='ITS3/Data/VH2'
+    txt_files2=glob.glob(dir_path2+'/*.txt')
+    BasgraphArray2=np.array([], dtype=TGraphErrors)
+    AmgraphArray2=np.array([], dtype=TGraphErrors)
+    FallgraphArray2=np.array([], dtype=TGraphErrors)
+    for file_name2 in txt_files2:
+        baseline2=np.array([])
+        baseline_err2=np.array([])
+        amplitude2=np.array([])
+        amplitude_err2=np.array([])
+        falltime2=np.array([])
+        falltime_err2=np.array([])
+        VH2=np.array([])
+        parts2=file_name2.split('j')
+        pixel2=parts2[1]
+        print(pixel2)
+        match2= re.search(r'(\d+)(?=.txt)',pixel2)
+        print(match2.group())
+        pixelArr2=np.append(pixelArr2, int(match2.group()))
+        index2=0
+        with open(file_name2,'r') as file:
+            for line in file:
+                data=line.split()
+                if index2==0:
+                    print(data[0])
+                baseline2=np.append(baseline2,float(data[0])*1000)
+                baseline_err2=np.append(baseline_err2,float(data[1])*1000)
+                amplitude2=np.append(amplitude2,float(data[2])*1000)
+                amplitude_err2=np.append(amplitude_err2,float(data[3])*1000)
+                falltime2=np.append(falltime2,float(data[4])*1.e9)
+                falltime_err2=np.append(falltime_err2,float(data[5])*1.e9)
+                VH2=np.append(VH2,0.4 + index2*0.1)
+                index2+=1
+        BasgraphArray2=np.append(BasgraphArray2,TGraphErrors(len(VH2), np.asarray(VH2, dtype=float), np.asarray(baseline2, dtype=float), np.zeros(len(VH2)), np.asarray(baseline_err2, dtype=float)))
+        AmgraphArray2=np.append(AmgraphArray2,TGraphErrors(len(VH2), np.asarray(VH2, dtype=float), np.asarray(amplitude2, dtype=float), np.zeros(len(VH2)), np.asarray(amplitude_err2, dtype=float)))
+        FallgraphArray2=np.append(FallgraphArray2,TGraphErrors(len(VH2), np.asarray(VH2, dtype=float), np.asarray(falltime2, dtype=float), np.zeros(len(VH2)), np.asarray(falltime_err2, dtype=float)))
+    for i in range(len(BasgraphArray2)):
+        SetGraph(BasgraphArray2[i], "gBaseline" + str(i), ";VH (V); Baseline (mV)", colorArr2[i], markerArr[i])
+        SetGraph(AmgraphArray2[i], "gAmplitude" + str(i), ";VH (V); Amplitude (mV)", colorArr2[i], markerArr[i])
+    #fit line
+    fitArray=[]
+    fitArray2=[]
+    ifit2=0
+    for indexfit in range(len(AmpliPx[0])):
+        fitArray.append(TF1("fit"+str(indexfit),"[0]+x*[1]",0,1.23))
+        fitArray2.append(TF1("fit"+str(indexfit),"[0]+x*[1]",0,1.23))
+        fitArray[indexfit].SetParameter(1,0)
+        fitArray2[indexfit].SetParameter(0,0)
+        fitArray[indexfit].SetLineColor(colorArr[indexfit])
+        fitArray2[indexfit].SetLineColor(colorArr[indexfit])
+        if(indexfit != 5 and indexfit != 6 and indexfit != 9 and indexfit != 10):
+            print(type(BasgraphArray[indexfit]))
+            BasgraphArray[indexfit].Fit(fitArray[indexfit],"rm+")
+            AmgraphArray[indexfit].Fit(fitArray2[indexfit],"rm+")
+            print("pixel" +str(indexfit) + " baseline fit par intercetta= "+ str(fitArray[indexfit].GetParameter(0))+" err="+ str(fitArray[indexfit].GetParError(0))+" coeff= "+str(fitArray[indexfit].GetParameter(1)) +" err="+ str(fitArray[indexfit].GetParError(1)))
+            print("pixel" +str(indexfit)+ " amplitude fit par intercetta= "+ str(fitArray2[indexfit].GetParameter(0))+" err="+ str(fitArray2[indexfit].GetParError(0))+" coeff= "+str(fitArray2[indexfit].GetParameter(1))+ " err="+ str(fitArray2[indexfit].GetParError(1)))
+        else:
+            BasgraphArray2[ifit2].Fit(fitArray[indexfit],"rm+")
+            AmgraphArray2[ifit2].Fit(fitArray2[indexfit],"rm+")
+            print("pixel scala a sx)" +str(indexfit)+ "baseline fit par intercetta= "+ str(fitArray[indexfit].GetParameter(0))+" coeff= "+str(fitArray[indexfit].GetParameter(1)))
+            print("pixel scala a sx)" +str(indexfit)+ "amplitude fit par intercetta= "+ str(fitArray2[indexfit].GetParameter(0))+" coeff= "+str(fitArray2[indexfit].GetParameter(1)))
+            ifit2+=1
+    canvas4 = TCanvas("canvas4","canvas4",2000,700)
+    canvas4.Divide(2,1)
+    canvas4.cd(1)
+    hFrame = canvas4.cd(1).DrawFrame(0.3,210,1.300,340,"Baseline vs VH; VH (V); Baseline (mV)")
+    legend = TLegend(0.15,0.55,0.35,0.9)
+    for i in range(len(AmpliPx[0])):
+        if( i==0 or i==7):
+            BasgraphArray[i].Draw("p,same")
+            fitArray[i].Draw("l,same")
+            legend.AddEntry(BasgraphArray[i], "Pixel "+str(int(i)))
+            legend.AddEntry(fitArray[i],"Linear fit, Pixel "+str(int(i)))
+        if(i==9):
+            BasgraphArray2[2].Draw("p,same")
+            fitArray[9].Draw("l,same")
+            legend.AddEntry(BasgraphArray2[2], "Pixel 6 ")#there is a shift of 1 pixel in the inner pixels so in position 9 in the 6
+            legend.AddEntry(fitArray[9], "Linear fit, Pixel 6 ")
+    legend.Draw()
+    text8 =TLatex(0.58, 0.8,"APTS_AO10P_B10")
+    text8.SetNDC()
+    text8.SetTextSize(gStyle.GetTextSize())
+    text.Draw()
+    canvas4.cd(2)
+    hFrame = canvas4.cd(2).DrawFrame(0.0,0,1.300,80,"Amplitude vs VH; VH (V); Amplitude (mV)")
+    legend2 = TLegend(0.15,0.55,0.35,0.9)
+    for i in range(len(AmpliPx[0])):
+        if(i==0 or i==7):
+            AmgraphArray[i].Draw("p,same")
+            fitArray2[i].Draw("l,same")
+            legend2.AddEntry(AmgraphArray[i], "Pixel "+str(int(i)))
+            legend2.AddEntry(fitArray2[i],"Linear fit, Pixel "+str(int(i)))
+        if(i==9):
+            AmgraphArray2[2].Draw("p,same")
+            fitArray2[9].Draw("l,same")
+            legend2.AddEntry(AmgraphArray2[2], "Pixel 6 ")
+            legend2.AddEntry(fitArray2[9], "Linear fit, Pixel 6 ")
+    legend2.Draw()
+    text9 =TLatex(0.50, 0.8,"APTS_AO10P_B10")
+    text9.SetNDC()
+    text9.SetTextSize(gStyle.GetTextSize())
+    text9.Draw()
+    canvas4.SaveAs('ITS3/data/output/VH_fit_pixel3.pdf')
+    
+        
+
